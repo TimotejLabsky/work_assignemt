bcd82145f0cf2e6003556255453f4937bd4f8c2f
Timotej Lábský <timotej.labsky@gmail.com>
2020-12-17 13:34:52 +0100

    Hello World

---
+++ src/main/java/com/labsky/timotej/BigShop.java
@@ -0,0 +1,11 @@
+package com.labsky.timotej;
+
+/**
+ * @author timotej
+ */
+public class BigShop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
