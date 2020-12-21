c9cdce0fee5cfcbf471006874746ae2e47861424
Timotej Lábský <timotej.labsky@gmail.com>
2020-12-21 15:34:44 +0100

    mandatory fields in SVK in receipt

--- src/main/java/com/labsky/timotej/model/Receipt.java
+++ src/main/java/com/labsky/timotej/model/Receipt.java
@@ -2,11 +2,16 @@ package com.labsky.timotej.model;
 
 import com.labsky.timotej.model.products.Product;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * @author timotej
  */
 public record Receipt(List<Product> products,
-                      double total) {
+                      String casRegisterUuid,
+                      LocalDateTime time,
+                      double total,
+                      String uuid,
+                      String contractorUuid) {
 }
