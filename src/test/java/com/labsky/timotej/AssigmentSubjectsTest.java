d5e4718d0613d3c6fcc135e071a0939290199516
Timotej Lábský <timotej.labsky@gmail.com>
2020-12-21 18:05:53 +0100

    simple tax test should work

--- src/test/java/com/labsky/timotej/AssigmentSubjectsTest.java
+++ src/test/java/com/labsky/timotej/AssigmentSubjectsTest.java
@@ -1,12 +1,22 @@
 package com.labsky.timotej;
 
+import com.labsky.timotej.exceptions.ProductNotFoundException;
+import com.labsky.timotej.model.Basket;
+import com.labsky.timotej.model.products.Product;
 import com.labsky.timotej.model.products.SimCard;
+import com.labsky.timotej.repository.ProductRepository;
+import com.labsky.timotej.repository.impl.ProductRepositoryImpl;
 import com.labsky.timotej.service.ProductService;
 import com.labsky.timotej.service.ReceiptService;
+import com.labsky.timotej.service.impl.ProductServiceImpl;
+import com.labsky.timotej.service.impl.ReceiptServiceImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -16,23 +26,28 @@ class AssigmentSubjectsTest {
 
     private static ProductService productService;
     private static ReceiptService receiptService;
+    private static final ProductRepository productRepository = ProductRepositoryImpl.getInstance();
 
-    private static final String[] basket = {"SimCard", "phone case", "phone insurance", "wired earphones", "wireless earphones"};
+    private static final Basket basket = new Basket(List.of("SIM card", "phone case", "phone insurance", "wired earphones", "wireless earphones"));
 
     @BeforeAll
     public static void init() {
-        productService = null;
-        receiptService = null;
+        productService = new ProductServiceImpl(productRepository);
+        receiptService = new ReceiptServiceImpl();
     }
 
     @Test
     void testTax() {
-        var product = productService.findByName("Sim card");
+        Product product = null;
+        try {
+            product = productService.findByName("SIM card");
+        } catch (ProductNotFoundException e) {
+            assertTrue(true, e.getMessage());
+        }
 
-        assertTrue(product.isPresent(), "product should not be empty");
-        var actualProduct = product.get();
-        assertTrue(actualProduct instanceof SimCard, "String \"Sim card\" should represent insurance");
-        assertEquals(actualProduct.getPrice() * 1.12, ((SimCard) actualProduct).getTax(), "tax should be on ");
+        assertNotNull(product, "product should not be empty");
+        assertTrue(product instanceof SimCard, "String \"Sim card\" should represent insurance");
+        assertEquals(2.4d, ((SimCard) product).getTax(), "tax should be on ");
     }
 
     @Test
@@ -42,6 +57,7 @@ class AssigmentSubjectsTest {
 
     @Test
     void testInsuranceDiscount() {
+
         assertTrue(false, "not implemented");
     }
 
