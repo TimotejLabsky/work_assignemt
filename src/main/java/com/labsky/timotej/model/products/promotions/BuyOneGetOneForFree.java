d3fa75bdeac2976aba28f4de5fe5a11d11d4be02
Timotej Lábský <timotej.labsky@gmail.com>
2020-12-21 18:25:39 +0100

    implementation of buy one get one for free

--- src/main/java/com/labsky/timotej/model/products/promotions/BuyOneGetOneForFree.java
+++ src/main/java/com/labsky/timotej/model/products/promotions/BuyOneGetOneForFree.java
@@ -1,14 +1,16 @@
 package com.labsky.timotej.model.products.promotions;
 
+import com.labsky.timotej.model.Basket;
 import com.labsky.timotej.model.products.Product;
 
 /**
  * @author timotej
  */
-public class BuyOneGetOneForFree implements SalePromotion{
+public class BuyOneGetOneForFree implements SalePromotion {
 
     @Override
-    public void apply(Product product) {
-
+    public void apply(Product product, Basket basket) {
+        basket.add(product);
+        product.getSalePromotions().add(new Discount(50d));
     }
 }
