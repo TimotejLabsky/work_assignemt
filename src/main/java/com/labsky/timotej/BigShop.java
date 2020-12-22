bf8ee7dd49a67bb019ef9094da90a77e9e72c633
Timotej Lábský <timotej.labsky@gmail.com>
2020-12-22 19:03:26 +0100

    implementation of print and Main - will need cleanup

--- src/main/java/com/labsky/timotej/BigShop.java
+++ src/main/java/com/labsky/timotej/BigShop.java
@@ -1,6 +1,7 @@
 package com.labsky.timotej;
 
-import com.labsky.timotej.model.products.Product;
+import com.labsky.timotej.exceptions.ProductNotFoundException;
+import com.labsky.timotej.model.Basket;
 import com.labsky.timotej.repository.ProductRepository;
 import com.labsky.timotej.repository.impl.ProductRepositoryImpl;
 import com.labsky.timotej.service.CashRegister;
@@ -8,12 +9,16 @@ import com.labsky.timotej.service.ProductService;
 import com.labsky.timotej.service.impl.CashRegisterImpl;
 import com.labsky.timotej.service.impl.ProductServiceImpl;
 
-import java.util.List;
+import java.util.Arrays;
+
+import static java.lang.System.err;
+import static java.lang.System.out;
 
 /**
  * @author timotej
  */
 public class BigShop {
+    public static final String INTERACTIVE_MODE = "-i";
 
     private final ProductService productService;
     private final CashRegister cashRegister;
@@ -25,5 +30,29 @@ public class BigShop {
     }
 
     public static void main(String[] args) {
+        BigShop bigShop = new BigShop();
+
+        if (Arrays.asList(args).contains(INTERACTIVE_MODE)) {
+            out.println("Might be implemented");
+        } else {
+            bigShop.nonInteractive(args);
+
+        }
+    }
+
+    private void nonInteractive(String[] products) {
+        Basket basket = new Basket();
+
+        for (String product : products) {
+            try {
+                basket.add(product);
+            } catch (ProductNotFoundException e) {
+                err.printf("Error: %s", e.getMessage());
+                return;
+            }
+        }
+
+        cashRegister.checkout(basket);
+
     }
 }
--- src/main/java/com/labsky/timotej/model/Receipt.java
+++ src/main/java/com/labsky/timotej/model/Receipt.java
@@ -3,8 +3,12 @@ package com.labsky.timotej.model;
 import com.labsky.timotej.model.products.Product;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * @author timotej
@@ -23,15 +27,77 @@ public record Receipt(
                 .sum();
     }
 
-    // TODO nice formated print
+
+    public static final int WIDTH = 40;
+    public static final String LOGO = """
+            _   _ _                          \s
+            | | | (_)_ __ ___   _ __ ___   ___\s
+            | |_| | | '__/ _ \\ | '_ ` _ \\ / _ \\
+            |  _  | | | |  __/ | | | | | |  __/
+            |_| |_|_|_|  \\___| |_| |_| |_|\\___|
+            \s
+            """;
+    public static final String companyAddress = """
+            Aardwark s.r.o
+            Trnavská cesta 84
+            821 01 Bratislava 2
+            Slovenská republika
+            """;
+    public static final String EMPLOYEE = "Timotej Lábský";
+
     @Override
     public String toString() {
-        return "Receipt{" +
-                "products=" + products +
-                ", casRegisterUuid=" + cashRegisterUuid +
-                ", time=" + time +
-                ", uuid=" + uuid +
-                ", contractorUui=" + contractorUui +
-                '}';
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+
+        return """
+                %s           
+                Company:
+                %s
+                Employee:
+                \tname: %s
+                \temail: timotej.labsky@aardwark.com
+                ---------------------------------------
+                Cash register id: 
+                %s
+                Date: %s
+                ---------------------------------------
+                PRODUCT\t\tCOUNT\tUNIT PRICE(incl. tax)
+                ---------------------------------------
+                %s
+                ---------------------------------------
+                TOTAL:
+                %.2f
+                """
+                .formatted(
+                        centerMultiLine(LOGO),
+                        centerMultiLine(companyAddress),
+                        EMPLOYEE,
+                        center(this.cashRegisterUuid.toString()),
+                        dateFormat.format(this.time),
+                        productsPrint(),
+                        getTotal()
+                );
+    }
+
+    private String productsPrint() {
+        return this.products.keySet().stream()
+                .map(product -> "%s\t%d\t\t%.2f %s%n".formatted(product.getName(),
+                        products.get(product),
+                        product.getPrice(),
+                        product.getCurrency()))
+                .collect(joining());
+    }
+
+    private String centerMultiLine(String s) {
+        return Arrays.stream(s.split("\n"))
+                .map(this::center)
+                .map(line -> line + "\n")
+                .collect(joining());
+    }
+
+    private String center(String s) {
+        return WIDTH > s.length()
+                ? " ".repeat((WIDTH - s.length()) / 2) + s + " ".repeat((WIDTH - s.length() + 1) / 2)
+                : s;
     }
 }
