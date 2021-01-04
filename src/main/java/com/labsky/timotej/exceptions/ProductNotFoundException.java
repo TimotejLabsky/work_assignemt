c09c22c0465973b33ff932c74227e7cc293ce30e
Timotej Lábský <tlabsky@gmail.com>
2021-01-04 13:47:56 +0100

    it is not Receipt related exception

--- src/main/java/com/labsky/timotej/exceptions/ProductNotFoundException.java
+++ src/main/java/com/labsky/timotej/exceptions/ProductNotFoundException.java
@@ -3,7 +3,7 @@ package com.labsky.timotej.exceptions;
 /**
  * @author timotej
  */
-public class ProductNotFoundException extends ReceiptValidationException{
+public class ProductNotFoundException extends Exception{
     public ProductNotFoundException(String message) {
         super(message);
     }
